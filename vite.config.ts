6725291bf348fdf43ffdc2ef4c25baa080e051c2
jfkeci <jakov-filip.sabolicek@almamedia.com>
2024-01-27 11:04:30 +0100

    chore: configure vite config build - increase chunkSizeWarningLimit

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "/gittify",
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          "react-chunk": ["react", "react-dom"],
+        },
+      },
+    },
+  },
 });
